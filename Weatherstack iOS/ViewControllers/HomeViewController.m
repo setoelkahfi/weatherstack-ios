7f6fcedccd12e703408291af8318a458debb5c91
Seto Elkahfi <setoelkahfi@gmail.com>
2019-12-11 01:03:53 +0100

    display favorites

--- Weatherstack iOS/ViewControllers/HomeViewController.m
+++ Weatherstack iOS/ViewControllers/HomeViewController.m
@@ -7,17 +7,48 @@
 //
 
 #import "HomeViewController.h"
+#import "StorageUtil.h"
 
 @interface HomeViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
 @implementation HomeViewController
 
+static NSString * CellIdentifier = @"HomeCell";
+
+#pragma mark - UI lifecycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
 }
 
+#pragma mark - UITableViewDelegate & UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [[[StorageUtil sharedInstance] getFavorites] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+        
+    NSDictionary * city = [[[StorageUtil sharedInstance] getFavorites] objectAtIndex:indexPath.row];
+        
+    cell.textLabel.text = [city objectForKey:@"name"];
+    cell.detailTextLabel.text = [city objectForKey:@"country"];
+        
+    return cell;
+}
 
 @end
